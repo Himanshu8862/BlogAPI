eab2e6dccf36f3466f067720df52d08c710822e9
Himanshu307 <himanshukumar.191cs122@nitk.edu.in>
2022-12-03 11:33:46 +0530

    setting up server

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+
+// middleware
+// routes
+
+
+// error handling middleware
+// listen to server
+
+const PORT = process.env.PORT || 9000
+app.listen(PORT, console.log(`Server is up and running on ${PORT}`));
